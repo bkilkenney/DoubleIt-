b98a5979a2f7f95e49a1c4b031551068905cfb1b
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-20 18:38:12 -0400

    Initial Commit

---
+++ DoubleIt()/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleIt__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            int x = int.Parse(Console.ReadLine());
+            x = DoubleIt(x);
+            Console.WriteLine("I've taken the liberty of doubling your number: " + x);
+        }
+        static int DoubleIt(int x)  
+        {
+            int result;
+            result = x * 2;
+            return result;
+        }
+    }
+}
+
+
